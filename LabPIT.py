f9c8dd412fa1d56d1280b3bcc2820d3d60cf3d37
Reef <reef.zayed2@gmail.com>
2023-10-25 13:30:03 +0300

    Done

---
+++ LabPIT.py
@@ -0,0 +1,14 @@
+from art import*
+from colorama import Fore, Back, Style
+
+tprint("BELIEVE AND ACHEIVE",font="block")
+tprint("HELLO",font="sub-zero")
+
+#Bonus
+text1=text2art("BELIEVE AND ACHEIVE",font="block")
+text2=text2art("HELLO",font="sub-zero")
+text3=text2art("Hi i'am Reef",font="cybermedium")
+
+print(Fore.RED + text1)
+print(Fore.CYAN + text2)
+print(Fore.MAGENTA + text3)
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# LAB_PIP
+# LAB_PIPART
 
 
 ## using what you've learned , create a new project to print Text Art in the terminal :
 - Create a virtual environment & activate it
-- Use ART package to print Text Art.
+- Use  package to print Text Art.
 - Print the phrase "BELIEVE AND ACHEIVE" designed with font `block`.
 - print the phrase "HELLO" designed with font `sub-zero`.
 
