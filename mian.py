07260f0ed109c9eb0efce0a08a17e6f8723920a5
lama1-abdullah <lama.alothemen21@gmail.com>
2023-10-25 14:20:15 +0300

    done lab+bon

--- mian.py
+++ mian.py
@@ -1,13 +1,16 @@
 from art import *
 from colorama import Fore, Back, Style
 
-tprint("test")
+tprint("BELIEVE AND ACHEIVE ",font="block") 
+tprint("HELLO",font="sub-zero")
+tprint("Hello test")
 tprint("test", font="bulbhead")
 tprint("test",font="random") 
-   
+
 
 print(Fore.RED + 'Hi my name is lama')
 print(Style.RESET_ALL)
 print(Back.GREEN + 'good moring')
 print(Style.RESET_ALL)
-print(Style.DIM + 'and in dim text')
+print(Style.DIM + 'add text')
+print(Style.RESET_ALL)
