5f16e1a1adef9f464215407017326beab659e0a2
naif <iimnaif@gmail.com>
2023-10-25 12:15:53 +0300

    lapp

---
+++ pipp.py
@@ -0,0 +1,5 @@
+from art import *
+Art=text2art("BELIEVE AND ACHEIVE",font='block',chr_ignore=True) # Return ASCII text with block font
+print(Art)
+Artt=text2art("HELLO",font='sub-zero',chr_ignore=True) # Return ASCII text with block font
+print(Artt)
