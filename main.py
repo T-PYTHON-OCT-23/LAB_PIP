30120358c678c35936d3d530e2364ed8300d89f6
Bader alotaibi <baderalotaibi3@gmail.com>
2023-10-25 12:02:20 +0300

    easy

---
+++ main.py
@@ -0,0 +1,10 @@
+from art import *
+
+phrase = "BELIEVE"
+tprint(phrase,font='block')
+phrase = "AND"
+tprint(phrase,font='block')
+phrase = "ACHEIVE"
+tprint(phrase,font='block')
+phrase = "HELLO"
+tprint(phrase,font='sub-zero')
