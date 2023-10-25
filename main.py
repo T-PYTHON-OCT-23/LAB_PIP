316982e452ac6a24c21a848f6eb82354c17723ec
Anoud <Anoudi90@icloud.com>
2023-10-25 13:46:50 +0300

    with bonus

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from art import *
-
+from colorama import *
 
 tprint("BELIEVE AND ACHEIVE" , font='block')
 tprint("HELLO" , font='sub-zero')
+
+
+
+
+print(Fore.LIGHTBLUE_EX + "let's design togther ... ")
+print(Style.RESET_ALL)
+print(Back.WHITE + "White")
+print(Style.RESET_ALL)
+
+
+
+
+    
+
+
+
+
+
