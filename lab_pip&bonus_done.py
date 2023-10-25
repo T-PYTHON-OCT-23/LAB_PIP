c23263a527ff8e0de24f191e64c89e11f30531be
Sufana_Aljawi <Sufanaaljawi@gmail.com>
2023-10-25 14:23:55 +0300

    Done

---
+++ lab_pip&bonus_done.py
@@ -0,0 +1,7 @@
+#Lab_PIP+Bonus
+from art import *
+from colorama import Fore, Back, Style
+tprint("BELIEVE AND ACHEIVE",font='block')
+tprint("HELLO",font ='sub-zero')
+print(Fore.CYAN)
+tprint("Sufana",font='random')
