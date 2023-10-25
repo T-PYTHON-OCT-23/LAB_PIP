94d9fff8fec6b5d9737306f3ac9da6b8c9fec222
AlenaziHazal <roomyham@MacBook-Pro.local>
2023-10-25 13:33:02 +0300

    bonus us solved

--- mian.py
+++ mian.py
@@ -4,3 +4,14 @@ Art = text2art("BELIEVE AND ACHEIVE",font='block')
 print(Art)
 Ar2 = text2art("Hello",font='sub-zreo')
 print(Ar2)
+Ar4 = text2art("Hello",font='random')
+print(Ar4)
+Ar3 = text2art("Hello",font='rand')
+print(Ar3)
+Ar5 = text2art("Hello",font='sub-zreo')
+print(Ar5)
+
+from colorama import Fore, Back
+
+print(Fore.CYAN,"Hello there")
+print(Back.RED,"ERORR")
