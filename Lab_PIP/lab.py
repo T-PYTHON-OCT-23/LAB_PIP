ef41621e803bba72406f9592eb1ed436537a5f89
AseelAlshammrani <AseelAlshammrani@gmail.com>
2023-10-25 13:30:08 +0300

    Done

---
+++ Lab_PIP/lab.py
@@ -0,0 +1,7 @@
+from art import *
+from colorama import Fore, Back, Style
+
+tprint("BELIEVE AND ACHEIVE",font='block')
+tprint('HELLO',font='sub-zero')
+print(Fore.MAGENTA)
+tprint('Aseel',font='random')
