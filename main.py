08697fab1f0961e313046f321a15ec6d1ca40d95
Sarah <sarahalqahtani763@gmail.com>
2023-10-25 14:10:57 +0300

    Lab16 DONE pip art package

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 #LAB 16
 from art import *
+from colorama import Fore, Back, Style
+#from colorama import *
 tprint("BELIEVE AND ACHEIVE", font = "block")
 tprint("HELLO", font = "sub-zero")
 
 #Bonus
-tprint("Sarah", font="random")
 tprint("Sarah", font="rnd-large")
 tprint("Sarah", font="rnd-xlarge")
-tprint("Sarah", font="wizard")
+tprint("SARAH", font = "block")
+
+print(Fore.BLUE + 'Sarah')
+print("\n")
+print(Back.WHITE + 'Sarah')
+print("\n")
+print(Style.DIM + 'Sarah')
+print(Style.RESET_ALL)
+
