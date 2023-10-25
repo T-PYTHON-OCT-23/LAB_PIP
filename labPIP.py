09bc21bee9cf710a979e525605919d13075c89a0
khamis_dossary <khamis.dossary@gmail.com>
2023-10-25 12:28:48 +0300

    All Done with the bonus

---
+++ labPIP.py
@@ -0,0 +1,15 @@
+from art import *
+from colorama import *
+
+tprint("BELIEVE\nAND\nACHEIVE",font="block")
+print("-----"*20)
+tprint("HELLO",font="sub-zero")
+print("-----"*20)
+
+
+#bonus
+print(Fore.RED+Back.WHITE+Style.BRIGHT)
+tprint(f" Real Madrid ",font="fancy",decoration="random",)
+
+print(Fore.YELLOW+Back.GREEN+Style.DIM,end="")
+tprint(" Man City ",font="fancy2",decoration="random")
