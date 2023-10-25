d16028f1a5a2efcb8802197ba61988560034a182
AlbandariAljabri <Albandarialjabri@gmail.com>
2023-10-25 14:00:58 +0300

    VirtualEnvironmentsLabSolved

---
+++ main.py
@@ -0,0 +1,20 @@
+from art import *
+from colorama import Fore, Back, Style
+
+ 
+tprint( "BELIEVE AND ACHEIVE", font="block" )
+print(Style.RESET_ALL)
+tprint("Hello" , font="sub-zero" )
+print()
+
+## Bonus
+
+
+tprint("Python camp at tweeq academy" ,font= "random")
+print(Fore.WHITE + Back.BLUE + "I love coffee" )
+print(Back.YELLOW + "This has a white background")
+print(Fore.GREEN + "PYTHON")
+
+
+
+
