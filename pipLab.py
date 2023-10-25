acbfa29b5006baafbc2cd0a4a9964aa453115a30
ebtesam <ebtesam167@hotmail.com>
2023-10-25 13:36:37 +0300

    solved bonus

--- pipLab.py
+++ pipLab.py
@@ -1,10 +1,14 @@
 from art import *
+from colorama import Fore, Back, Style
+
 
 text1 = text2art("BELIEVE AND ACHIEVE", "block")
-print(text1)
+print(Fore.LIGHTMAGENTA_EX , text1)
 
+print(Style.RESET_ALL)
 text2 = text2art("HELLO", "sub-zero")
-print(text2)
+print(Back.MAGENTA, text2 )
+print(Style.RESET_ALL)
 
 
 
