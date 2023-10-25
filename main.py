c904043bb174cd280c44bd928a85ab2cef5a1152
Sarah <sarahalqahtani763@gmail.com>
2023-10-25 13:15:03 +0300

    LAB16 compleated

---
+++ main.py
@@ -0,0 +1,10 @@
+#LAB 16
+from art import *
+tprint("BELIEVE AND ACHEIVE", font = "block")
+tprint("HELLO", font = "sub-zero")
+
+#Bonus
+tprint("Sarah", font="random")
+tprint("Sarah", font="rnd-large")
+tprint("Sarah", font="rnd-xlarge")
+tprint("Sarah", font="wizard")
