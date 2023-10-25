5af862b2b8112b6f2c7e90a45c9e5cb0b7726392
oudy yousef <oudy.usef@gmail.com>
2023-10-25 13:15:35 +0300

    solved bonus

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from art import * 
+from colorama import Back
 
 tprint("BELIEVE AND ACHEIVE",font="block")
 tprint("HELLO",font="sub-zero")
+
+
+print("------bouns------")
+
+Art1=text2art("HELLO WORD","random") # random font mode
+print(Art1)
+
+Art=text2art("HELLO WORD","random") # random font mode
+print(Back.GREEN+ Art)
+
