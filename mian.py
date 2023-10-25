658097b55960ddf764d6f87e3571805d624ac450
lama1-abdullah <lama.alothemen21@gmail.com>
2023-10-25 13:49:33 +0300

    Done Lab+ Bonus

---
+++ mian.py
@@ -0,0 +1,13 @@
+from art import *
+from colorama import Fore, Back, Style
+
+tprint("test")
+tprint("test", font="bulbhead")
+tprint("test",font="random") 
+   
+
+print(Fore.RED + 'Hi my name is lama')
+print(Style.RESET_ALL)
+print(Back.GREEN + 'good moring')
+print(Style.RESET_ALL)
+print(Style.DIM + 'and in dim text')
