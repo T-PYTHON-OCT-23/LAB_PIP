dbedee3422ee4c7342d75f0e926c6051cb36deae
Reef <reef.zayed2@gmail.com>
2023-10-25 13:35:14 +0300

    Done Lab PIP

--- LabPIT.py
+++ LabPIT.py
@@ -3,12 +3,12 @@ from colorama import Fore, Back, Style
 
 tprint("BELIEVE AND ACHEIVE",font="block")
 tprint("HELLO",font="sub-zero")
-
-#Bonus
+tprint("Hi i'am Reef",font="cybermedium")
+#Bonus solved 
 text1=text2art("BELIEVE AND ACHEIVE",font="block")
 text2=text2art("HELLO",font="sub-zero")
 text3=text2art("Hi i'am Reef",font="cybermedium")
 
 print(Fore.RED + text1)
 print(Fore.CYAN + text2)
-print(Fore.MAGENTA + text3)
+print(Fore.MAGENTA + text3) 
