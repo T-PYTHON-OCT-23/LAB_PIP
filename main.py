d2eb8412b86572819f94d555a9f907d2188c5b73
abdullah <abdullah.alanezi.23@gmail.com>
2023-10-25 13:15:32 +0300

    solved bonus

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 
-
 from art import *
+from colorama import Fore,Style,Back
 
 
 tprint("BELIEVE AND ACHEIVE",font="blok")
 
 tprint("HELLO",font='sub-zero')
+
+tprint("hi there",font='small' )
+
+tprint("Hi my name is Abdullah",font='cybermedium')
+
+print(Fore.RED + 'Hi there')
+print(Back.GREEN + 'my name is abdullah')
+print(Style.DIM + 'I learned how to install colorama package and use it')
