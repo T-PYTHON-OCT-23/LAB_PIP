6180a6619f34242f1798f31d2ecda21aaeb29a45
MaramAlasmari <147453012+MaramAlasmari@users.noreply.github.com>
2023-10-25 13:44:47 +0300

    done

---
+++ lab/lab.py
@@ -0,0 +1,11 @@
+from art import *
+from colorama import Fore, Back, Style
+aprint("happy")
+print(Fore.MAGENTA+ "MARAM ALASAMARI")
+tprint("maram",font="bulbhead")
+tprint("maram", font="block")
+tprint("maram", font="sub-zero")
+tprint("Maram<3")
+
+
+
