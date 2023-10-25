33a3129d52c9f2348c5cbea05a1874025abe51cd
AlenaziHazal <roomyham@MacBook-Pro.local>
2023-10-25 13:24:36 +0300

    main sloved

---
+++ mian.py
@@ -0,0 +1,6 @@
+#Enter python3 in terminal then paste the code below
+from art import *
+Art = text2art("BELIEVE AND ACHEIVE",font='block')
+print(Art)
+Ar2 = text2art("Hello",font='sub-zreo')
+print(Ar2)
