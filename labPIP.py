9655a41ff82c9000ee621e9c60a5a39b33a82aa0
khamis_dossary <khamis.dossary@gmail.com>
2023-10-25 14:28:55 +0300

    some adds

--- labPIP.py
+++ labPIP.py
@@ -1,7 +1,7 @@
 from art import *
 from colorama import *
 
-tprint("BELIEVE\nAND\nACHEIVE",font="block")
+tprint("BELIEVE\nAND\nACHEIVE",font="blocks")
 print("-----"*20)
 tprint("HELLO",font="sub-zero")
 print("-----"*20)
@@ -10,5 +10,8 @@ print("-----"*20)
 #bonus
 text1=text2art(" Real Madrid ",font="fancy",decoration="random",)
 print(Fore.RED+Back.WHITE+Style.BRIGHT+text1+Style.RESET_ALL)
-text2=text2art(" Man City ",font="fancy2",decoration="random")
-print(Fore.YELLOW+Back.GREEN+Style.DIM+text2+Style.RESET_ALL)
+print("-----"*20)
+text2=text2art("Man City",font="rnd")
+print(Fore.BLACK+Back.CYAN+Style.DIM+text2+Style.RESET_ALL)
+
+print("test")
